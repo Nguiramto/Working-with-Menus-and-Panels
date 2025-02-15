0aee0a55f9855c717f87c11185633648863c5ffb
Nguiramto <douglasmbainaissem05@gmail.com>
2025-02-15 14:36:18 +0300

    Commit #1
    Here we are working with modules. We've created the "SimpleFrameManagement" and inside it we've made the "SimpleFrame" java class in which we have created a Frame to handle the rest of the components of our application about to be created.

---
+++ SimpleFrameManagement/src/main/java/org/example/Main.java
@@ -0,0 +1,8 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) {
+        SimpleFrame simpleFrame = new SimpleFrame();
+        simpleFrame.showIt();
+    }
+}
---
+++ SimpleFrameManagement/src/main/java/org/example/SimpleFrame.java
@@ -0,0 +1,22 @@
+package org.example;
+
+import javax.swing.*;
+
+public class SimpleFrame extends JFrame {
+
+    public SimpleFrame() {
+
+        this.setSize(600,600);
+        this.setLocation(600,600);
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        JFrame.setDefaultLookAndFeelDecorated(true);
+
+    }
+
+    //--------Making the Frame visible and setting title----------
+    public void showIt() {
+        this.setTitle("Graphical User Interface");
+        this.setVisible(true);
+    }
+
+}
